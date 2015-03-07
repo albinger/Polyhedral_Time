b8ffa3778370e45b679e6604e79d628ae493e9c3
Andrew Albinger <andrew.albinger@gmail.com>
2015-03-06 22:58:22 -0600

    fixed major memory leaks
    the original TextWatch code had a memory leak.  I believe I've fixed that.

--- src/TextWatch.c
+++ src/TextWatch.c
@@ -4,7 +4,11 @@
 #define DEBUG 0
 #define BUFFER_SIZE 44
 
-  
+//#ifdef DEBUG
+#define APP_LOG_D app_log(level, __FILE_NAME__, __LINE__, fmt, ## args)
+//#else
+//#define APP_LOG_D do { } while(0)
+//#endif  
 static Window *window;
 
 static BitmapLayer *s_background_layer;
@@ -20,7 +24,7 @@ typedef struct {
 
 static Line line1;
 static Line line2;
-static Line line3;
+
 
 static struct tm *t;
 static GFont lightFont;
@@ -28,42 +32,62 @@ static GFont boldFont;
 
 static char line1Str[2][BUFFER_SIZE];
 static char line2Str[2][BUFFER_SIZE];
-static char line3Str[2][BUFFER_SIZE];
+
+int pauseAnimation;
 
 // Animation handler
+//static void animationStoppedHandler(struct Animation *animation, bool finished, void *context, Line *line)
 static void animationStoppedHandler(struct Animation *animation, bool finished, void *context)
 {
-	Layer *textLayer = text_layer_get_layer((TextLayer *)context);
-	GRect rect = layer_get_frame(textLayer);
-	rect.origin.x = 144;
-	layer_set_frame(textLayer, rect);
+
+  property_animation_destroy((PropertyAnimation *) context);
+  //APP_LOG(APP_LOG_LEVEL_INFO,"animation_destroy");
+//	Layer *textLayer = text_layer_get_layer((TextLayer *)context);
+//	GRect rect = layer_get_frame(textLayer);
+//	rect.origin.x = 144;
+//	layer_set_frame(textLayer, rect);
 }
 
 // Animate line
 static void makeAnimationsForLayers(Line *line, TextLayer *current, TextLayer *next)
 {
+  if (pauseAnimation){return;}
 	GRect fromRect = layer_get_frame(text_layer_get_layer(next));
 	GRect toRect = fromRect;
-	toRect.origin.x -= 144;
-	
+	//toRect.origin.x -= 144;
+	fromRect.origin.x=144;
+  toRect.origin.x = 0;
+  //APP_LOG(APP_LOG_LEVEL_INFO,"1");
 	line->nextAnimation = property_animation_create_layer_frame(text_layer_get_layer(next), &fromRect, &toRect);
-	animation_set_duration((Animation *)line->nextAnimation, 400);
-	animation_set_curve((Animation *)line->nextAnimation, AnimationCurveEaseOut);
+	//APP_LOG(APP_LOG_LEVEL_INFO,"2");
+  animation_set_duration((Animation *)line->nextAnimation, 400);
+	//APP_LOG(APP_LOG_LEVEL_INFO,"3");
+  animation_set_curve((Animation *)line->nextAnimation, AnimationCurveEaseOut);
+  //APP_LOG(APP_LOG_LEVEL_INFO,"4");
+  animation_set_handlers((Animation *)line->nextAnimation, (AnimationHandlers){
+    .stopped = (AnimationStoppedHandler)animationStoppedHandler}, line->nextAnimation);
+  //APP_LOG(APP_LOG_LEVEL_INFO,"4.5");
 	animation_schedule((Animation *)line->nextAnimation);
-	
+	//APP_LOG(APP_LOG_LEVEL_INFO,"5");
 	GRect fromRect2 = layer_get_frame(text_layer_get_layer(current));
+  //APP_LOG(APP_LOG_LEVEL_INFO,"6");
 	GRect toRect2 = fromRect2;
-	toRect2.origin.x -= 144;
-	
+	//toRect2.origin.x -= 144;
+	fromRect2.origin.x = 0;
+  toRect2.origin.x = -144;
+  //APP_LOG(APP_LOG_LEVEL_INFO,"7");
 	line->currentAnimation = property_animation_create_layer_frame(text_layer_get_layer(current), &fromRect2, &toRect2);
-	animation_set_duration((Animation *)line->currentAnimation, 400);
-	animation_set_curve((Animation *)line->currentAnimation, AnimationCurveEaseOut);
-	
+	//APP_LOG(APP_LOG_LEVEL_INFO,"8");
+  animation_set_duration((Animation *)line->currentAnimation, 400);
+	//APP_LOG(APP_LOG_LEVEL_INFO,"9");
+  animation_set_curve((Animation *)line->currentAnimation, AnimationCurveEaseOut);
+	//APP_LOG(APP_LOG_LEVEL_INFO,"10");
 	animation_set_handlers((Animation *)line->currentAnimation, (AnimationHandlers) {
 		.stopped = (AnimationStoppedHandler)animationStoppedHandler
-	}, current);
-	
+	}, line->currentAnimation);
+	//APP_LOG(APP_LOG_LEVEL_INFO,"11");
 	animation_schedule((Animation *)line->currentAnimation);
+  //APP_LOG(APP_LOG_LEVEL_INFO,"12");
 }
 
 // Update line
@@ -80,13 +104,16 @@ static void updateLineTo(Line *line, char lineStr[2][BUFFER_SIZE], char *value)
 		memset(lineStr[1], 0, BUFFER_SIZE);
 		memcpy(lineStr[1], value, strlen(value));
 		text_layer_set_text(next, lineStr[1]);
+    //APP_LOG(APP_LOG_LEVEL_INFO,"tlst");
 	} else {
 		memset(lineStr[0], 0, BUFFER_SIZE);
 		memcpy(lineStr[0], value, strlen(value));
 		text_layer_set_text(next, lineStr[0]);
+    //APP_LOG(APP_LOG_LEVEL_INFO,"tlst");
 	}
 	
 	makeAnimationsForLayers(line, current, next);
+  //APP_LOG(APP_LOG_LEVEL_INFO,"end ULT");
 }
 
 // Check to see if the current line needs to be updated
@@ -106,36 +133,46 @@ static bool needToUpdateLine(Line *line, char lineStr[2][BUFFER_SIZE], char *nex
 // Update screen based on new time
 static void display_time(struct tm *t)
 {
-	// The current time text will be stored in the following 3 strings
+	// The current time text will be stored in the following 2 strings
 	char textLine1[BUFFER_SIZE];
 	char textLine2[BUFFER_SIZE];
-	char textLine3[BUFFER_SIZE];
+	
 	int ShowDie;
   
-	ShowDie = time_to_3words(t->tm_hour, t->tm_min, textLine1, textLine2, textLine3, BUFFER_SIZE);
+  //APP_LOG(APP_LOG_LEVEL_INFO,"call ttw");
+	ShowDie = time_to_2words(t->tm_hour, t->tm_min, textLine1, textLine2, BUFFER_SIZE);
+  //APP_LOG(APP_LOG_LEVEL_INFO, "got1:%s:", textLine1);
+  //APP_LOG(APP_LOG_LEVEL_INFO, "got2:%s:", textLine2);
 	
   
   layer_set_hidden(bitmap_layer_get_layer(s_background_layer), ShowDie);
-  
+  //APP_LOG(APP_LOG_LEVEL_INFO, "hidden");
   
 	if (needToUpdateLine(&line1, line1Str, textLine1)) {
+        //APP_LOG(APP_LOG_LEVEL_INFO,"nTUL");
 		updateLineTo(&line1, line1Str, textLine1);	
+    //APP_LOG(APP_LOG_LEVEL_INFO,"uLT");
 	}
-	if (needToUpdateLine(&line2, line2Str, textLine2)) {
+//APP_LOG(APP_LOG_LEVEL_INFO,"line1");
+
+  if (needToUpdateLine(&line2, line2Str, textLine2)) {
+   // APP_LOG(APP_LOG_LEVEL_INFO,"nTUL");
 		updateLineTo(&line2, line2Str, textLine2);	
+    //APP_LOG(APP_LOG_LEVEL_INFO,"uLT");
 	}
-	if (needToUpdateLine(&line3, line3Str, textLine3)) {
-		updateLineTo(&line3, line3Str, textLine3);	
-	}
+  //APP_LOG(APP_LOG_LEVEL_INFO,"line2");
 }
 
 // Update screen without animation first time we start the watchface
 static void display_initial_time(struct tm *t)
 {
-	time_to_3words(t->tm_hour, t->tm_min, line1Str[0], line2Str[0], line3Str[0], BUFFER_SIZE);
+	time_to_2words(t->tm_hour, t->tm_min, line1Str[0], line2Str[0], BUFFER_SIZE);
 	text_layer_set_text(line1.currentLayer, line1Str[0]);
 	text_layer_set_text(line2.currentLayer, line2Str[0]);
-	text_layer_set_text(line3.currentLayer, line3Str[0]);
+	
+  //APP_LOG(APP_LOG_LEVEL_INFO, "got1:%s:", line1Str[0]);
+  //APP_LOG(APP_LOG_LEVEL_INFO, "got2:%s:", line2Str[0]);
+  
 }
 
 // Debug methods. For quickly debugging enable debug macro on top to transform the watchface into
@@ -193,14 +230,29 @@ static void configureLightLayer(TextLayer *textlayer)
 // Time handler called every minute by the system
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 	t = tick_time;
+  //APP_LOG(APP_LOG_LEVEL_INFO, "tick");
   display_time(tick_time);
 }
 
+void focus_handler(bool in_focus) {
+  if (in_focus) {
+    //APP_LOG(APP_LOG_LEVEL_INFO, "App window is back in focus");
+    pauseAnimation = 0;
+    display_time(t);
+  } else {
+    //APP_LOG(APP_LOG_LEVEL_INFO, "App window is out of focus");
+    pauseAnimation = 1;
+  }
+}
+
 static void init() {
+  pauseAnimation = 0;
   window = window_create();
   window_stack_push(window, true);
   window_set_background_color(window, GColorBlack);
 
+  
+  app_focus_service_subscribe(focus_handler);
   
   // seed random
   srand(time(NULL));
@@ -208,7 +260,7 @@ static void init() {
   
   //Create GBitmap, then set to created BitmapLayer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_D20);
-  s_background_layer = bitmap_layer_create(GRect(22, 50, 102, 168));
+  s_background_layer = bitmap_layer_create(GRect(22, 47, 102, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_set_hidden(bitmap_layer_get_layer(s_background_layer), 1);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
@@ -230,12 +282,7 @@ static void init() {
 	configureLightLayer(line2.currentLayer);
 	configureLightLayer(line2.nextLayer);
 
-	// 3rd layers
-	line3.currentLayer = text_layer_create(GRect(0, 85, 144, 50));
-	line3.nextLayer = text_layer_create(GRect(144, 85, 144, 50));
-	configureLightLayer(line3.currentLayer);
-	configureLightLayer(line3.nextLayer);
-
+	
 	// Configure time on init
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
@@ -247,9 +294,7 @@ static void init() {
 	layer_add_child(window_layer, text_layer_get_layer(line1.nextLayer));
 	layer_add_child(window_layer, text_layer_get_layer(line2.currentLayer));
 	layer_add_child(window_layer, text_layer_get_layer(line2.nextLayer));
-	layer_add_child(window_layer, text_layer_get_layer(line3.currentLayer));
-	layer_add_child(window_layer, text_layer_get_layer(line3.nextLayer));
-
+	
 	#if DEBUG
 	// Button functionality
 	window_set_click_config_provider(window, (ClickConfigProvider) click_config_provider);
@@ -258,8 +303,11 @@ static void init() {
   tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 }
 
+            
 static void deinit() {
+  //APP_LOG(APP_LOG_LEVEL_INFO,"deinit called");
 	tick_timer_service_unsubscribe();
+  app_focus_service_unsubscribe();
 	window_destroy(window);
 }
 
--- src/num2words-en.c
+++ src/num2words-en.c
@@ -3,19 +3,17 @@
 #include "pebble.h"
 
 
-  
-  
-int time_to_words(int hours, int minutes, char* words, size_t length) {
+int time_to_2words(int hours, int minutes, char *line1, char *line2, size_t length)
+{
 
-  
-  memset(words, 0, length);
-  char hrs[10];
+	//ShowDie = time_to_words(hours, minutes, value, length);
+	char hrs[10];
   char mns[10];
   
   int hrand;
   int mrand;
   
-  static const int const DICE[] = {
+  static int const DICE[] = {
     4,
     6,
     8,
@@ -27,17 +25,10 @@ int time_to_words(int hours, int minutes, char* words, size_t length) {
     8,
     6,
     4
-};
-  
-  //srand(time(NULL));
-  
-  //d4, d6, d8, d10, d12
-  
+  };
   
   hrand = DICE[rand()%5];
   mrand = DICE[rand()%11];
-  
- 
   
   if(hours/hrand==0){
     snprintf(hrs,9,"1d%i-%i",hrand,hrand-hours);
@@ -48,51 +39,29 @@ int time_to_words(int hours, int minutes, char* words, size_t length) {
       snprintf(hrs,9,"%id%i",(hours/hrand),hrand);
     }
   }
-  if(minutes/mrand==0){
-    snprintf(mns,9,"1d%i-%i",mrand,mrand-minutes);
-  }else{ 
-    if(minutes%mrand != 0){
-      snprintf(mns,9,"%id%i+%i",(minutes/mrand),mrand,(minutes%mrand));
-    }else{
-      snprintf(mns,9,"%id%i", (minutes/mrand),mrand);
+  if(minutes == 0){
+    snprintf(mns,9," ");
+  }else{
+    if(minutes/mrand==0){
+      snprintf(mns,9,"1d%i-%i",mrand,mrand-minutes);
+    }else{ 
+      if(minutes%mrand != 0){
+        snprintf(mns,9,"%id%i+%i",(minutes/mrand),mrand,(minutes%mrand));
+      }else{
+        snprintf(mns,9,"%id%i", (minutes/mrand),mrand);
+      }
     }
   }
-  snprintf(words,length,"%s %s 0",hrs,mns);  
-  APP_LOG(APP_LOG_LEVEL_INFO,"TTW:%s:",words);
-   if(mrand == 20){return(0); }else{return(1);}
-}
+  memset(line1, '\0', length);
+  memset(line2, '\0', length);
+  //snprintf(line1,strlen(hrs),"%s ",hrs);  
+  memcpy(line1,hrs,strlen(hrs));
+  //snprintf(line2,strlen(mns),"%s ",mns); 
+  memcpy(line2,mns,strlen(mns));
+  //APP_LOG(APP_LOG_LEVEL_INFO, "hrs:%s:", hrs);
+  //APP_LOG(APP_LOG_LEVEL_INFO, "mns:%s:", mns);
+  
 
-int time_to_3words(int hours, int minutes, char *line1, char *line2, char *line3, size_t length)
-{
-	char value[length];
-  int ShowDie;
-	ShowDie = time_to_words(hours, minutes, value, length);
-	
-	memset(line1, 0, length);
-	memset(line2, 0, length);
-	memset(line3, 0, length);
-	
-	char *start = value;
-	char *pch = strstr (start, " ");
-	while (pch != NULL) {
-		if (line1[0] == 0) {
-			memcpy(line1, start, pch-start);
-		}  else if (line2[0] == 0) {
-			memcpy(line2, start, pch-start);
-		} else if (line3[0] == 0) {
-			memcpy(line3, start, pch-start);
-		}
-		start += pch-start+1;
-		pch = strstr(start, " ");
-	}
-	
-	// Truncate long teen values
-	if (strlen(line2) > 7) {
-		char *pch = strstr(line2, "teen");
-		if (pch) {
-			memcpy(line3, pch, 4);
-			pch[0] = 0;
-		}
-	}
-  return(ShowDie);
+
+  if(mrand == 20){return(0);}else{return(1);}
 }
